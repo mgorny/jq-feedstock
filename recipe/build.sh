e1f03b42321c73f4ca088dcd707d430f73c5b5d0
Michał Górny <mgorny@gentoo.org>
2026-09-08 13:56:21 +0200

    fixup! skip not win

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Get an updated config.sub and config.guess
-if [[ ${target_platform} == win-* ]]; then
-  patch_libtool
-else
+if [[ ${target_platform} != win-* ]]; then
   cp $BUILD_PREFIX/share/gnuconfig/config.* ./vendor/oniguruma
   cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
 fi
@@ -12,6 +10,10 @@ set -ex
 chmod +x configure
 
 ./configure --prefix=$PREFIX --with-oniguruma=$PREFIX
+
+if [[ ${target_platform} != win-* ]]; then
+  patch_libtool
+fi
 
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
